120cea8c199c372486d64e0745fe93b873f5c866
Perry Theobald <perrytjr@gmail.com>
2020-09-22 13:00:48 -0700

    employeeclass and start of serverjs file

---
+++ server.js
@@ -0,0 +1,5 @@
+const mysql = requirer("mysql");
+const Department = require("./lib/department");
+const Employee = require("./lib/employee");
+const Role = require("./lib/role");
+const inquirer = require("inquirer");
