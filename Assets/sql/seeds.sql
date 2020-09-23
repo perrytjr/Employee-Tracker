85d2e97a0e280fca724c2443dff4c25fd9a467e4
Perry Theobald <perrytjr@gmail.com>
2020-09-22 22:54:53 -0700

    started inquirer section

--- Assets/sql/seeds.sql
+++ Assets/sql/seeds.sql
@@ -6,11 +6,11 @@ USE employeedata_DB;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT, 
-    name VARCHAR(30) NOT NULL,
+    [name] VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
+CREATE TABLE [role] (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
--- server.js
+++ server.js
@@ -3,3 +3,65 @@ const Department = require("./lib/department");
 const Employee = require("./lib/employee");
 const Role = require("./lib/role");
 const inquirer = require("inquirer");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function manageEmployees(){
+    inquirer.prompt([
+        {
+        type: "list",
+        name: "makeAdjustment",
+        message: "What adjustment would you like to make to your employee information?",
+        choices: [
+            "Add department",
+            "Add roles",
+            "Add employee",
+            "View departments",
+            "View roles",
+            "View employees",
+            "Update employee roles"
+        ]
+        }
+    ]).then(function(yourChoice) {
+        switch(yourChoice.makeAdjustment) {
+            case "Add department":
+                addDepartment();
+                break;
+                case "Add roles":
+                    addRoles();
+                    break;
+                case "Add employee":
+                    addEmployee();
+                    break;
+                case "View departments":
+                    viewDepartment();
+                    break;
+                case "View Roles":
+                    viewRoles();
+                    break;
+                case "View employees":
+                    viewEmployees();
+                    break;
+                case "Update employee roles":
+                    updateemployeeRoles();
+                    default:
+                        buildteamPage(); //figure this out tmrw maybe connection end not sure yet. 
+        }
+
+    });
+}
