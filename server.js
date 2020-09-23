160e03982af5a640dd82d34e4e1e80aa1e5d2b4c
Perry Theobald <perrytjr@gmail.com>
2020-09-23 12:00:57 -0700

    connection to server good and commands showed up in termina after running node server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const mysql = requirer("mysql");
+const mysql = require("mysql");
 const Department = require("./lib/department");
 const Employee = require("./lib/employee");
 const Role = require("./lib/role");
