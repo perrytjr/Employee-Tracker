f1c46cb0062064f98377b792f8aa1092faa56197
Perry Theobald <perrytjr@gmail.com>
2020-09-22 17:57:15 -0700

    startof_db

---
+++ Assets/sql/seeds.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS employeedata_DB;
+
+CREATE DATABASE employeedata_DB;
+
+USE employeedata_DB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT, 
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR (30) NOT NULL, 
+    last_name VARCHAR (30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
+
+
